5e69ff2285e45c0bacae0f89146d30af3d3e049d
vprimachenko <vprimachenko@ya.ru>
2013-08-04 16:52:29 +0200

    make plugin working regardless of sublime version

--- keepOpen.py
+++ keepOpen.py
@@ -2,5 +2,10 @@ import sublime, sublime_plugin
 
 class keepOpen(sublime_plugin.EventListener):
     def on_close(self, view):
-       if len(sublime.windows()) == 1 and len(sublime.windows()[0].views()) == 0:
-       		sublime.windows()[0].new_file()
+        # get version via api change
+        sv = 2
+        if hasattr(sublime.Window,"lookup_symbol_in_index"):
+            sv = 3
+
+        if len(sublime.windows()) == 1 and len(sublime.windows()[0].views()) == [43,1,1,0][sv]:
+            sublime.windows()[0].new_file()
