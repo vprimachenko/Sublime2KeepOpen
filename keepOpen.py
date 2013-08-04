ef93be23d5481e49e1f7c180b999f120f5cc4277
vprimachenko <vprimachenko@ya.ru>
2013-08-04 16:30:18 +0200

    ST3 support
    change according to new api

--- keepOpen.py
+++ keepOpen.py
@@ -2,5 +2,5 @@ import sublime, sublime_plugin
 
 class keepOpen(sublime_plugin.EventListener):
     def on_close(self, view):
-       if len(sublime.windows()) == 1 and len(sublime.windows()[0].views()) == 1:
+       if len(sublime.windows()) == 1 and len(sublime.windows()[0].views()) == 0:
        		sublime.windows()[0].new_file()
